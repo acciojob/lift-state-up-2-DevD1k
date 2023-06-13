f4881db932c6a4cdf86462e26a4c72cd084a458c
Devdutt Mehrol <devduttmehrol666@gmail.com>
2023-06-13 16:38:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import ParentComponent from "./ParentComponent";
+import ChildComponent from "./ChildComponent";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ParentComponent />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/ChildComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ChildComponent = ({ inputValue, setInputValue }) => {
+  return (
+    <div className="child">
+      <h1>Child Component</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default ChildComponent;
